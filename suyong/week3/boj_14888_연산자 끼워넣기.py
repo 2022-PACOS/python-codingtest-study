15604f2d16f9b41329330a855a495246f7113ba0
maplesuyong <suyong0507@gmail.com>
2022-09-01 15:40:15 +0900

    [Baekjoon-14888] 연산자 끼워넣기

---
+++ suyong/week3/boj_14888_연산자 끼워넣기.py
@@ -0,0 +1,56 @@
+import sys
+from itertools import permutations
+
+
+n = int(sys.stdin.readline())
+nums = []
+temp = sys.stdin.readline().split()
+for i in range(n):
+    nums.append(int(temp[i]))
+op = []
+temp = sys.stdin.readline().split()
+for i in range(len(temp)):
+    op.append(int(temp[i]))
+
+per_tot = 0
+for i in op:
+    per_tot += i
+
+ops = []
+for i in range(len(op)):
+    if i == 0:
+        for j in range(op[i]):
+            ops.append('+')
+    elif i == 1:
+        for j in range(op[i]):
+            ops.append('-')
+    elif i == 2:
+        for j in range(op[i]):
+            ops.append('*')
+    else:
+        for j in range(op[i]):
+            ops.append('//')
+
+solve = []
+for i in permutations(ops, per_tot):
+    solve.append(i)
+
+a = list(set(solve))
+print(a)
+
+result = []
+for i in a:
+    strings = str(nums[0])
+    for j in range(len(i)):
+        if int(strings) < 0 and i[j] == "//":
+            string_temp = i[j] + str(nums[j+1])
+            strings = str(-(abs((int(strings))) // nums[j+1]))
+        else:
+            string_temp = i[j] + str(nums[j+1])
+            strings += string_temp
+            strings = str(eval(strings))
+    print(strings)
+    result.append(int(strings))
+
+print(max(result))
+print(min(result))
