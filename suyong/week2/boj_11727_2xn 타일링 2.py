caf8e80c156ba73f9cd4aa91f3c6f3528ec4be5b
Ahjeong Park <qkrdkwjd59@naver.com>
2022-09-02 22:45:09 +0900

    처음부터 다시시작

---
+++ suyong/week2/boj_11727_2xn 타일링 2.py
@@ -0,0 +1,26 @@
+# https://www.acmicpc.net/problem/11727
+# 해설: https://yongyongtv.tistory.com/17
+# 백준 11727번 2xn 타일링
+# 메모리: 실패
+# 시간: 실패
+# 코드길이: 227B
+
+import sys
+import math
+n = int(sys.stdin.readline())
+answer = 0
+
+for i in range((n//2) + 1):
+    temp = (math.factorial(n-i) / (math.factorial(n-2*i) * math.factorial(i))) * math.pow(2, i)
+    answer += int(temp)
+print(answer)
+
+
+# # 모범답안
+# table = [0, 1, 3]
+#
+# n = int(input())  # 입력값을 변수에 저장
+# for i in range(3, n+1):
+#     table.append(table[i-1] + 2*table[i-2])
+#
+# print(table[n])  # n번째 index로 접근
