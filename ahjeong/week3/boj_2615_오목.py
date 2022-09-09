1ff96900b46cb60074674018206a9cb298a135e7
Ahjeong Park <qkrdkwjd59@naver.com>
2022-09-09 13:35:16 +0900

    [Baekjoon-2615] - 오목.py

--- ahjeong/week3/boj_2615_오목.py
+++ ahjeong/week3/boj_2615_오목.py
@@ -1,64 +1,37 @@
 import sys
+
 # 바둑판 초기화
 matrix = [list(map(int, sys.stdin.readline().split(' '))) for _ in range(19)]
 
-# 런타임 에러
-# import sys
-# # 바둑판 초기화
-# matrix = [list(map(int, sys.stdin.readline().split(' '))) for _ in range(19)]
+# (x, y) 현재 위치에서 오른쪽 위, 오른쪽, 오른쪽 아래, 아래 방향 
+dir = [(-1, 1), (0, 1), (1, 1), (1, 0)]
 
-# def check(case, i, j):
-#     for _ in range(5):
-#         hori = case
-#         if matrix[i][j] == matrix[i][j+1]:
-#             hori.append([i, j+1])
-#             j += 1
-#         else:
-#             break
-#     for _ in range(5):
-#         vert = case
-#         if matrix[i][j] == matrix[i+1][j+1]:
-#             vert.append([i+1, j+1])
-#             i += 1
-#             j += 1
-#         else:
-#             break
-#     for _ in range(5):
-#         diag = case
-#         if matrix[i][j] == matrix[i+1][j]:
-#             diag.append([i+1, j])
-#             i += 1
-#         else:
-#             break
-#     return max(hori, vert, diag) 
+def check(i, j):
+    for d in dir:
+        # 이동한 방향 좌표 구하기
+        x, y = i + d[0], j+d[1]
+        cnt = 1
+        # 이동한 좌표의 범위 확인, 이동 전 좌표 == 이동 후 좌표 인지?
+        while 0 <= x < 19 and y < 19 and matrix[x][y] == matrix[i][j]:
+            cnt += 1
+            x += d[0]
+            y += d[1]
+            if cnt == 5:
+                # 오목이 완성 된 후 양쪽 끝에 같은 색의 바둑알이 있는지 확인 -> 6개 이상이 되는 것을 방지
+                if not (0 <= x < 19 and y < 19 and matrix[x][y] == matrix[i][j]):
+                    if not (0 <= i-d[0] < 19 and 0 <= j-d[1] < 19 and matrix[i-d[0]][j-d[1]] == matrix[i][j]):
+                        print(matrix[i][j]) #이긴 바둑알의 번호를 출력
+                        print(i+1, j+1)
+                        exit(0)
+                    else:
+                        break
+                else:
+                    break
 
-# def getResult():
-#     case1 = []
-#     case2 = []
-#     for i in range(19):
-#         for j in range(19):
-#             if matrix[i][j] == 1:
-#                 case = [[i, j]]
-#                 case1 = check(case, i, j)
-#             elif matrix[i][j] == 2:
-#                 case = [[i, j]]
-#                 case2 = check(case, i, j)
-#             else:
-#                 continue
-#             # 검은색와 흰색이 동시에 이기는 경우가 없고 각각 두 군데 이상에서 동시에 이기는 경우는 입력으로 들어오지 않음
-#             if len(case1) == 5 or len(case2) == 5: 
-#                 return case1, case2
+for i in range(19):
+    for j in range(19):
+        if matrix[i][j] != 0:
+            check(i, j)
 
-# case1, case2 = getResult()
-
-# if len(case1) == 5 and len(case2) != 5:
-#     print(1)
-#     i = case1[0][0] + 1
-#     j = case1[0][1] + 1
-#     print(i, j)
-# elif len(case1) != 5 and len(case2) == 5:
-#     print(2)
-#     i = case2[0][0] + 1
-#     j = case2[0][1] + 1
-# else:
-#     print(0)
+# check 함수에서 승부를 결정하지 않았을 경우 0을 출력함.
+print(0)
