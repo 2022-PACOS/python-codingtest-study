62d3efd988fe8522926d39c6455a0128ff430699
Ahjeong Park <qkrdkwjd59@naver.com>
2022-10-11 14:14:11 +0900

    [Baekjoon-11399] - ATM.py

---
+++ ahjeong/week6/boj_11399_ATM.py
@@ -0,0 +1,15 @@
+# 정렬
+
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+P = list(map(int, input().split()))
+
+P.sort()
+
+answer = 0
+for i in range(1, N+1):
+    answer += sum(P[:i])
+
+print(answer)
